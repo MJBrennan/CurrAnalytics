a7d0e9929d6528d9e71bbddedc27ccefcec0725c
MJBrennan <mike7149@gmail.com>
2017-07-14 15:34:54 +0100

    Menu Items

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
                                             Logout
                                         </a>
 
-                                         <a href="account">
+                                         <a href="allentries">
                                             Account
                                         </a>
 
